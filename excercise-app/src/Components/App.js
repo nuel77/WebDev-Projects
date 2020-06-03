25b7d214892da5e11c2f18ac1d5770524912fc16
Emmanuel Thomas Devasia <50878033+emmanuel010@users.noreply.github.com>
2020-06-03 20:33:06 +0530

    comments to es8 method Object.entries

--- excercise-app/src/Components/App.js
+++ excercise-app/src/Components/App.js
@@ -9,18 +9,29 @@ class App extends Component {
   };
 
   getExcercisesByMuscles() {
-    return this.state.exercises.reduce((accumilator, exercise) => {
-      const { muscles } = exercise;
+    /*
+    Object.entries is ES8 fucntion to convert Object returned by reduce method to an array 
+    where each element is an array with the first element as key and second element as the value
+    of the Object
+    */
+    return Object.entries(
+      /*
+      loops over all the elements in the excercices array and creates an object 
+      with the muscles as key and value as an array of excercies related to 
+      that muscle
+      */
+      this.state.exercises.reduce((accumilator, exercise) => {
+        const { muscles } = exercise;
+        // check and add excercise which affects the muscle
+        accumilator[muscles] = accumilator[muscles]
+          ? [...accumilator[muscles], exercise]
+          : [exercise];
 
-      accumilator[muscles] = accumilator[muscles]
-        ? [...accumilator[muscles], exercise]
-        : [exercise];
-
-      return accumilator;
-    }, {});
+        return accumilator;
+      }, {}) //inital value of accumilator
+    );
   }
   render() {
-    console.log(this.getExcercisesByMuscles());
     return (
       <Fragment>
         <Header></Header>
