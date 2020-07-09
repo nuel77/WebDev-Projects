376365a7aaf20d6da457ccc9e2896352003c92f1
Emmanuel Thomas Devasia <50878033+emmanuel010@users.noreply.github.com>
2020-07-09 19:52:32 +0530

    database development

--- website-pinner/server/index.js
+++ website-pinner/server/index.js
@@ -1,8 +1,54 @@
-const express= require("express")
-const passport=require("passport")
-const cors= require("cors")
-const app= express()
+const express = require("express")
+const bodyParser = require('body-parser');
+const cors = require("cors")
+const findOrCreate = require('mongoose-findorcreate')
+const app = express()
 app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://emmanuel:9495112289@cluster0-j7mp0.mongodb.net/webpager?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const userDataSchema = new mongoose.Schema({
+    url: String,
+    title: String,
+    description: String,
+    category: String
+})
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    name: String,
+    userData: [userDataSchema]
+})
+userSchema.plugin(findOrCreate);
+
+const userData = mongoose.model("userData", userDataSchema)
+const user = mongoose.model("user", userSchema)
+
+app.post("/login", (req, res) => {
+    const {name, email} = req.body
+    const newUser = new user({
+        email: email,
+        name: name,
+        userData: []
+    })
+    user.find({ email:email }, (err,userData)=>{
+        if(!userData.length){
+            newUser.save()
+            console.log("not found creating new")
+        }else{
+           res.send(userData)
+        }
+    });
+
+})
 
 
-app.listen(8080,()=>{console.log("app listening 8080")})
+app.listen(8080, () => {
+    console.log("app listening 8080")
+})
--- website-pinner/src/Components/Home/index.jsx
+++ website-pinner/src/Components/Home/index.jsx
@@ -1,108 +1,134 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import {makeStyles} from '@material-ui/core/styles';
-import {CssBaseline, Typography, Button, Paper, Divider} from "@material-ui/core";
+import {CssBaseline, Typography, Button, Box, Divider} from "@material-ui/core";
 import {GoogleLogin} from 'react-google-login';
 import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props'
 import FacebookIcon from '@material-ui/icons/Facebook';
+const axios= require('axios')
 
 const useStyles = makeStyles((theme) => ({
     root: {
         height: "100vh",
-        backgroundImage: `url("https://assets.website-files.com/5bfd1275cc56e15ce750b18e/5c289afb9a157511e8893a43_19.%20Can%20Can.jpg")`,
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
 
     },
     gridRow: {
         height: "100%",
         color: "white",
-        marginTop:"10%",
-        marginBottom:"2%"
+        marginTop: "10%",
+        marginBottom: "2%"
     },
     LoginButton: {
         width: "16rem",
         height: "3.5rem",
         fontWeight: "normal",
         justifyContent: "center",
-        margin: "10px"
+        margin: "10px",
     },
-    divider:{
-      height:"4px",
-      backgroundColor:"white",
-        borderRadius:"50px"
+    divider: {
+        height: "4px",
+        backgroundColor: "white",
+        borderRadius: "50px"
     },
-    backgroundDiv:{
-
-        borderRadius:"16px"
+    backgroundDiv: {
+        backgroundImage: `url("https://assets.website-files.com/5bfd1275cc56e15ce750b18e/5c289afb9a157501b4893a89_55.%20My%20Pink.jpg")`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        height:"100vh",
+        color:"#263238"
+    },
+    rightContentWrapper: {
+        textAlign: "center"
+    },
+    leftContentWrapper: {
+        alignItems:"center",
+       color:"white"
+    },
+    webpager:{
+       color: "#9575cd"
     }
+
 }))
 const Home = (props) => {
     const classes = useStyles()
 
-    const responseGoogle = (response) => {
-        console.log(response);
-        props.history.push("app")
+    const responseGoogle = async (response) => {
+        const data= response.profileObj
+        const res= await axios.post("http://localhost:8080/login",data)
+        // props.history.push("app")
     }
 
-    const responseFacebook = (response) => {
+    const responseFacebook = async (response) => {
         console.log(response);
-        props.history.push("app")
+        const res= await axios.post("http://localhost:8080/login",response)
+        // props.history.push("app")
     }
 
     return (
         <div className={classes.root}>
-            <Grid container alignContent="center">
-                <Grid container justify="center" className={classes.gridRow}>
-                    <Grid item sm={5}>
-                        <div className={classes.backgroundDiv}>
-                        <Typography align="center" variant={"h2"}>Welcome to <span style={{color:"#3F51B5"}}> Webpager!</span></Typography>
-                            <Divider variant="middle" className={classes.divider}/>
-                            <Typography variant={"h5"}>
-                            <ul>
-                                <li>Are you tired of managing your cluttered bookmarks?</li>
-                                <li>Wish you could store all of them somewhere easy to
-                                    access?
-                                </li>
-                                <li>
-                                    Introducing Webpager! your one stop solution for storing and
-                                    organizing all your important
-                                    links in one place. Login to check it out
-                                </li>
-                            </ul>
-                        </Typography>
+            <Grid container alignItems="center">
+                <Grid item sm={7} >
+                    <Box className={classes.rightContentWrapper}>
 
-                        </div>
-                    </Grid>
+                        <Typography align="center" variant={"h2"}>Welcome to <span
+                          className={classes.webpager}> WEBPAGER!</span></Typography>
+
+                        <Divider variant="middle" className={classes.divider}/>
+
+                        <GoogleLogin
+                            className={classes.LoginButton}
+                            clientId="513756828470-4dndgt7608ql784k15n3b5htme6domeg.apps.googleusercontent.com"
+                            buttonText="LOGIN WITH GOOGLE"
+                            onSuccess={responseGoogle}
+                            onFailure={responseGoogle}
+                            // isSignedIn={true}           //will call on success callback on load
+                            cookiePolicy={'single_host_origin'}
+                        />
+                        <FacebookLogin
+                            appId="1485709034963671"
+                            // autoLoad={true}
+                            fields="name,email,picture"
+                            icon="fa-facebook"
+                            callback={responseFacebook}
+                            render={renderProps => (
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    startIcon={<FacebookIcon/>}
+                                    className={classes.LoginButton}>
+                                    LOGIN WITH FACEBOOK
+                                </Button>
+                            )}
+                        />
+
+                    </Box>
                 </Grid>
-                <Grid container justify="center">
-                    <GoogleLogin
-                        className={classes.LoginButton}
-                        clientId="513756828470-4dndgt7608ql784k15n3b5htme6domeg.apps.googleusercontent.com"
-                        buttonText="LOGIN WITH GOOGLE"
-                        onSuccess={responseGoogle}
-                        onFailure={responseGoogle}
-                        // isSignedIn={true}           //will call on success callback on load
-                        cookiePolicy={'single_host_origin'}
-                    />
-                    <FacebookLogin
-                        appId="1485709034963671"
-                        // autoLoad={true}
-                        fields="name,email,picture"
-                        icon="fa-facebook"
-                        callback={responseFacebook}
-                        render={renderProps => (
-                            <Button
-                                variant="contained"
-                                color="primary"
-                                startIcon={<FacebookIcon/>}
-                                className={classes.LoginButton}>
-                                LOGIN WITH FACEBOOK
-                            </Button>
-                        )}
-                    />
+
+                <Grid item sm={5} >
+                        <Grid container className={classes.backgroundDiv} alignItems="center">
+                            <Typography variant={"h6"}>
+                                <ul>
+                                    <li>Are you tired of managing your cluttered bookmarks?</li>
+                                    <li>Wish you could store all of them somewhere easy to
+                                        access?
+                                    </li>
+                                    <br/>
+                                    <li>
+                                        Introducing Webpager! <br/>
+                                        Your one stop solution for storing and
+                                        organizing all your important
+                                        links in one place.
+                                    </li>
+                                    <li>
+                                        Just login to check it out
+                                    </li>
+                                </ul>
+                            </Typography>
+                        </Grid>
                 </Grid>
+
+
             </Grid>
         </div>
     )
